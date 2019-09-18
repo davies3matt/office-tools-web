b1ad04659c554bac6e533a0d158d9a280802aa4c
Matt <24929239@student.g.nwu.ac.za>
2019-09-18 11:50:36 +0200

    first commit

---
+++ src/app/desktop-page/desktop-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-page',
+  templateUrl: './desktop-page.component.html',
+  styleUrls: ['./desktop-page.component.css']
+})
+export class DesktopPageComponent implements OnInit {
+
+  constructor() { }
+
+  clickFileTransfer() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
